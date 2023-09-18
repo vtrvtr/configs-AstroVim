f825b5057e4ee8b822139cdfa4391abed52381b6
Vitor Hugo <vitorh@al.com.au>
2023-09-18 14:42:42 +1000

    Added catppuccin. Added rust pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,8 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.python" },
 }
