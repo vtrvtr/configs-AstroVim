d0e995381b75ab2a95b3b3062f5b17169f2a675f
Vitor Hugo <vitorh@al.com.au>
2023-09-18 14:42:10 +1000

    Zen mode now has metadata

--- plugins/zen.lua
+++ plugins/zen.lua
@@ -8,8 +8,8 @@ return {
       width = function() return math.min(120, vim.o.columns * 0.75) end,
       height = 0.9,
       options = {
-        number = false,
-        relativenumber = false,
+        number = true,
+        relativenumber = true,
         foldcolumn = "0",
         list = false,
         showbreak = "NONE",
@@ -24,7 +24,7 @@ return {
     },
     on_open = function() -- disable diagnostics and indent blankline
       vim.g.diagnostics_mode_old = vim.g.diagnostics_mode
-      vim.g.diagnostics_mode = 0
+      -- vim.g.diagnostics_mode = 0
       vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[0])
       vim.g.indent_blankline_enabled_old = vim.g.indent_blankline_enabled
       vim.g.indent_blankline_enabled = false
