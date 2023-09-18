87af0c0f2c8a0bcf47d2d6ddeced36450ef6fe14
Vitor Hugo <vitorh@al.com.au>
2023-09-18 14:42:30 +1000

    Increased reformat delay

--- plugins/auto-save.lua
+++ plugins/auto-save.lua
@@ -2,7 +2,7 @@ return {
   'Pocco81/auto-save.nvim',
   event = "BufEnter",
   opts = function(_, config) 
-    config.debounce_delay = 150
+    config.debounce_delay = 1000
     config.trigger_events = {"InsertLeave"}
     return config -- return final config table
   end,
