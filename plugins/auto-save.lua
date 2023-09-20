8905a6570444c2f867132018144e5de655375f02
Vitor Hugo <vitorh@al.com.au>
2023-09-20 16:18:32 +1000

    Minor auto save change

--- plugins/auto-save.lua
+++ plugins/auto-save.lua
@@ -1,9 +1,9 @@
 return {
   'Pocco81/auto-save.nvim',
   event = "BufEnter",
-  opts = function(_, config) 
+  opts = function(_, config)
     config.debounce_delay = 1000
-    config.trigger_events = {"InsertLeave"}
+    -- config.trigger_events = {"InsertLeave"}
     return config -- return final config table
   end,
 }
