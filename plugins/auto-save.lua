cd662a176f7cad34dac78fb45ea68c1bb6e550d3
Vitor Hugo <vitorh@al.com.au>
2023-10-23 08:25:41 +1100

    Stopping auto save from saying something at every save

--- plugins/auto-save.lua
+++ plugins/auto-save.lua
@@ -3,6 +3,9 @@ return {
   event = "BufEnter",
   opts = function(_, config)
     config.debounce_delay = 1000
+    config.execution_message = {
+      message = function() return "" end,
+    }
     -- config.trigger_events = {"InsertLeave"}
     return config -- return final config table
   end,
